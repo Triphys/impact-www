fb8fcf39f1b58f8e6b007e82f9831701c717744a
Kenneth Illman <kenneth.illman@gmail.com>
2020-03-06 16:31:24 +0530

    Header adjustments

--- components/slices/impact-heading.vue
+++ components/slices/impact-heading.vue
@@ -1,26 +1,16 @@
 <template>
-
   <section class="slice-impact-heading impact-content">
     <div class="gc">
 
-
       <div class="g-10 g-push-1 slice-content">
-       
-
-
-        <div class="content-text">
-
+        <div class="content-text" :class="{'-text-center' : sliceRaw.primary.heading[0].type === 'heading2'}">
           <prismic-rich-text :field="sliceRaw.primary.heading"/>
-
         </div>
       </div>
-
-
 
     </div>
 
   </section>
-
 </template>
 
 <script>
