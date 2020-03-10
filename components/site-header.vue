714cfbad76722c1d0f4faead82c51c4c8b415fef
Kenneth Illman <kenneth.illman@gmail.com>
2020-03-10 09:45:38 +0530

    Header in progress

--- components/site-header.vue
+++ components/site-header.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="site-header">
+  <div class="site-header" :class="{'-no-logo' : this.$route.name === 'startpage'}">
 
     <!-- <div class="site-home" @click="closeMenu">
       <nuxt-link to="/" ><svg-icon class="logo" name="activetalents-filled" /></nuxt-link>
@@ -15,47 +15,91 @@
       </div>
     </div>
 
-<!--     <div class="menu">
-      <ul @click="navigateMenu"> -->
+    <!-- {{menu}}<br><br> -->
+
+
+    <div class="menu">
+      <!-- <ul @click="navigateMenu"> -->
+        <a  href="/" class="logo-link">
+          <svg-icon class="logo" name="impact" />
+        </a>
+      <ul>
+
+        <li><div>Träning</div>
+
+          <ul class="sub">
+            <li>
+              <a href="/">Thaiboxning</a>
+            </li>
+            <li>
+              <a href="/">PT</a>
+            </li>
+          </ul>
+
+        </li>
+        <li>
+            <a href="/">Schema</a>
+        </li>
+        <li>
+            <a href="/">Priser</a>
+        </li>
+        <li>
+            <a href="/">Om Oss</a>
+        </li>
+        <li>
+            <a href="/">Hitta Hit/Kontak</a>
+        </li>
      
 
-        <!--  <li v-for="(item, index) in menu.body" :key="'b-' + index">
+       <!--  <li v-for="(item, index) in menu" :key="'i-' + index"> -->
 
-          
-          {{item.primary.label1[0].text}}
-          {{item.items}} <br> -->
-         <!--  <br>
+<!--           <br>
+          {{item.primary.label}}
+          <prismic-link :field="item.primary.link">{{item.primary.label[0].text}}</prismic-link>
 
-            <div v-for="(item2, index) in item.items" :key="'a-' + index">
-              {{item2.link_3.uid}} <br><br>
-            </div> -->
+          <br>
+          <br> -->
+          <!-- <a :href="item.href"></a> -->
+     <!--      {{item.items}} <br>
+          <br>
 
-        <!--   </li> -->
+          <div v-for="(item2, index) in item.items" :key="'a-' + index">
+            {{item2.link_3.uid}} <br><br>
+          </div> -->
+        </li>
 
-     <!--    <li><span class="header">Active talents</span></li>
-        <li><nuxt-link  to="/about" >About</nuxt-link></li>
-        <li><nuxt-link  to="/services" >Services</nuxt-link></li>-->
-<!--         <li><nuxt-link  to="/content/mahatma-gandhi" >Gandhi</nuxt-link></li>
-        <li><nuxt-link  to="/content/nelson-mandela" >Mandela</nuxt-link></li> 
-        <li><nuxt-link  to="/event" >Event</nuxt-link></li>
-        <li><nuxt-link  to="/contact" >Contact</nuxt-link></li>
+        <!--   
+          <li><nuxt-link  to="/content/mahatma-gandhi" >Gandhi</nuxt-link></li>
+          <li><nuxt-link  to="/content/nelson-mandela" >Mandela</nuxt-link></li> 
+          <li><nuxt-link  to="/event" >Event</nuxt-link></li>
+          <li><nuxt-link  to="/contact" >Contact</nuxt-link></li> 
+        -->
+
       </ul>
-    </div> -->
+    </div>
 
+    <div class="social-links">
+      <a :href="setfb" target="_blank"><svg-icon class="logo-icon" name="facebook" /></a>
+      <a :href="setin" target="_blank"><svg-icon class="logo-icon" name="instagram" /></a>
+    </div>
 
   </div>
 
 </template>
 
+
 <script>
 
+
+
 export default {
- //props: ['menu'],
   name: 'site-header',
+  props: ['menu','setfb','setin'],
   components: {
   },
   data() {
     return {
+      sm: this.settings,
       isMenuOpen: this.$store.getters.getMenu,
     };
   },
@@ -69,13 +113,15 @@ export default {
     },
     // navigateMenu: function(){
     //   this.$store.commit("setMenuNavigate",true)
-
     //   setTimeout(() => {
     //     this.$store.commit("setMenuNavigate",false)
     //     this.closeMenu();
     //   }, 425);
 
     // }
+  },
+  mounted() {
+    console.log(this.$route.name)
   }
 }
 </script>
@@ -90,36 +136,9 @@ export default {
   left: 0;
   z-index: 20;
 
-  .site-home {
-    border-radius: 400px;
-    width: 64px;
-    height: 64px;
-    background-color: $black;
-    position: fixed;
-    top: 16px;
-    left: 16px;
-    z-index: 222;
-    transition: all .275s ease;
-
-    svg {
-      fill: $white;
-      height: 40px;
-      width: 40px;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -56%);
-    }
-
-    &:hover{
-      background-color: $black;
-    }
-
-
-
-
+  .social-links {
+    display: none;
   }
-
 
 
   .menu-toggle {
@@ -131,21 +150,30 @@ export default {
 
     .burger {
       position: relative;
-      margin: 32px 27px;
-      width: 42px;
-      text-align: right;
-      direction: rtl;
+      margin: 14px;
+      width: 30px;
+      text-align: center;
       cursor: pointer;
       z-index: 5;
 
       span {
-        height: 5px;
+        height: 3px;
         display: block;
         margin-bottom: 7px;
         background: $black;
-        transition: all .4s ease-out;
+        transition: all .325s ease-out;
         position: relative;
         width: 100%;
+        margin-left: auto;
+        margin-right: auto;
+        
+        &:nth-child(1) {
+          top: 0;
+        }
+        &:nth-child(3) {
+         bottom: 0;
+         top: auto;
+        }
       }
 
       &:hover {
@@ -164,53 +192,69 @@ export default {
     width: 100%;
     overflow: scroll;
     opacity: 0;
-    span {
-      opacity: 0;
-      transition: all .225s ease-in-out;
+    background: $yellow;
+    padding: 10px 24px 24px;
+    text-align: center;
+
+    .logo {
+      width: 100px;
+      height: 33px;
+      margin-top: 7px;
+      &:hover {
+        fill: $grey;
+        cursor: pointer;
+      }
     }
-    a {
-      opacity: 0;
-      transition: all .225s ease-in-out;
+    ul a, 
+    ul div {
+      display: block;
+      text-decoration: none;
+      font-size: 28px;
+      line-height: 1.1;
+      font-family: $font-impact;
+      border-bottom: 1px solid $black;
+      padding: 11px 0 12px;
+      &:hover {
+        color: $grey;
+      }
+    }
+
+    > ul {
+       margin: 21px 0;
+       border-top: 1px solid $black;
+    }
+
+    ul.sub {
+      display: none;
+      &.-show {
+        display: block;
+      }
+      a {
+        color: $grey;
+        border-bottom: none;
+        padding-bottom: 0;
+      }
+      padding-bottom: 14px;
+      border-bottom: 1px solid $black;
     }
   }
 
   ul {
-    padding: 50px 11px 0 ;
-    margin: 0 auto;
-    width: 60%;
+  
     text-align: center;
-
+    padding: 0;
+    margin: 0;
+    
     li {
       margin: 0;
       padding: 0;
       list-style-type: none;
+
     }
 
-    .header {
-      font-family: 'Oswald', sans-serif;
-      text-transform: uppercase;
-      color: $black;
-      display: inline-block;;
-      margin: 50px 0 0 4px;
-      font-size: 14px;
-    }
 
-    a {
-      display: block;
-      padding: 10px 10px;
-      line-height: 1;
-      white-space: nowrap;
-      background: $black;
-      margin-top: 6px;
-      text-decoration: none;
-      border-radius: 9999px;
-    }
 
   }
-
-
-
-
 
   .-menu-open &{
 
@@ -224,16 +268,18 @@ export default {
           &:nth-child(1) {
             width: 100%;
             transform: rotate(135deg);
-            top: 12px;
+            top: 10px;
           }
           &:nth-child(2) {
-            width: 100%;
+            width: 0;
             opacity: 0;
+            transition: all .275s ease-out;
+
           }
           &:nth-child(3) {
             width: 100%;
             transform: rotate(-135deg);
-            bottom: 12px;
+            bottom: 10px;
           }
         }
       }
@@ -253,38 +299,90 @@ export default {
       opacity: 1;
       visibility: visible;
       z-index: 4;
-      span {
-        opacity: 1;
-        transition: all .45s ease-in-out;
+    }
 
+  }
+
+  @include VP1024 {
+
+    &.-no-logo {
+      .logo-link {
+        display: none;
+
+      }
+      .menu {
+        background: none;
+      }
+    }
+   
+
+    .social-links {
+      display: block;
+      float: right;
+      margin: 14px 21px 0 0;
+      svg {
+        width: 40px;
+        height: 40px;
+        margin: 7px 7px 0;
       }
       a {
-        opacity: 1;
-        transition: all .75s ease-in-out;
+        display: inline-block;
+        &:hover {
+          svg {
+            fill: $grey;
+          }
+        }
       }
     }
 
-  }
+    .menu-toggle {
+      display: none;
+    }
 
-  .-menu-open.-portrait &{
-    .menu-background {
-      height: 200vh;
-      width: 200vh;
-      top: -50vh;
-      right: -50vh;
+    .menu {
+      
+
+      .logo-link {
+         position: absolute;
+         top: 18px;
+         left: 21px;
+      }
+
+      .logo {
+       
+        margin-top: 0;
+        
+        width: 120px;
+        height: 38px;
+      }
+
+      display: block;
+      visibility: visible;
+      opacity: 1;
+      padding: 14px 28px 17px;
+      height: auto;
+      li {
+        display: inline-block;
+        margin-left: 10px;
+        margin-right: 10px;
+      }
+      ul a,
+      ul div  {
+        border: none;
+      }
+      > ul {
+        margin: 0;
+       border-top: none;
+    }
+
+    ul.sub {
+        a {
+          border-bottom: none;
+        }
+        border-bottom: none;
+      }
     }
   }
-
-  a {
-    display: block;
-  }
-
-
-
-
-
-
-
 
 }
 
