a7adf2435118223a131ea734900fad7e01ec58e3
Kenneth Illman <kenneth.illman@gmail.com>
2020-03-16 09:13:06 +0530

    added preview page

---
+++ pages/preview.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>Loading the Prismic's Preview...</p>
+</template>
+
+<script>
+
+import Prismic from "prismic-javascript"
+import LinkResolver from "~/plugins/link-resolver.js"
+// import PrismicConfig from "~/prismic.config.js"
+
+export default {
+  name: 'Preview',
+  async asyncData({ query, redirect }) {
+
+    let previewToken = query.token
+
+    console.log('previewToken -> ' + previewToken); // Undefined <--- !! 
+
+    const api = await Prismic.getApi(process.env.PRISMIC_API)
+
+    const url = await api.previewSession(previewToken, LinkResolver, '/preview')
+    console.log('url -> ' + url);
+    redirect(url)
+  },
+}
+</script>
