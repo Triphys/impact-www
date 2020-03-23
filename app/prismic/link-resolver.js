42710ded80f0d8d5b44754716d4c2ba333dbb766
Kenneth Illman <kenneth.illman@gmail.com>
2020-03-23 08:20:36 +0530

    added files

---
+++ app/prismic/link-resolver.js
@@ -0,0 +1,25 @@
+/**
+ * To learn more about Link Resolving check out the Prismic documentation
+ * https://prismic.io/docs/vuejs/beyond-the-api/link-resolving
+ */
+
+export default function (doc) {
+  if (doc.isBroken) {
+    return '/not-found'
+  }
+
+  if (doc.type === 'startpage') {
+    return '/'
+  }
+
+  if (doc.type === 'content') {
+    return `/content/${doc.uid}`
+  }
+
+  if (doc.type === 'preview') {
+    return '/preview'
+  }
+
+  return '/'
+  //return '/not-found'
+}
