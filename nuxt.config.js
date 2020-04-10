8a8a0443ca3f83a29db106ddc3df0fe3c2998c2c
Kenneth Illman <kenneth.illman@gmail.com>
2020-04-10 13:05:58 +0530

    try

--- nuxt.config.js
+++ nuxt.config.js
@@ -100,9 +100,9 @@ module.exports = {
  // generate: {
  //    fallback: '404.html' // Netlify reads a 404.html, Nuxt will load as an SPA
  //  },
-  /*
-
-  */
+    generate: {
+    fallback: '404.html' // Netlify reads a 404.html, Nuxt will load as an SPA
+  },
 
   /*
   ** Build configuration
