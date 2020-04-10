d57230c4f27a81e6c94f7a23c66a680276622003
Kenneth Illman <kenneth.illman@gmail.com>
2020-04-10 09:30:27 +0530

    Removed debug class

--- components/slices/impact-day.vue
+++ components/slices/impact-day.vue
@@ -27,7 +27,6 @@ export default {
   mounted() {
       if (process.client) {
         if (document.querySelector('.nuxt-link-handler')){
-          document.querySelector('.nuxt-link-handler').classList.add("-debug-comp")
           document.querySelector('.nuxt-link-handler').addEventListener('click', event => {
             event.preventDefault()
             this.$router.push(event.target.pathname)
