91cec1b775220ee1487dc7483dc65cb4f643a96d
Kenneth Illman <kenneth.illman@gmail.com>
2020-05-23 09:38:19 +0530

    Styleguide re-added

---
+++ pages/styleguide.vue
@@ -0,0 +1,386 @@
+<template>
+<section class="p-design-delivery site-grid">
+
+
+    <!-- TOP HERO -->
+
+    <div class="sgd-s-hero">
+      <div>
+
+        <!-- <svg-icon class="logo" name="impact" /> -->
+
+        <h2 class="title">
+         Styleguide
+        </h2>
+
+        <h6 class="title -sub">
+          impactgym.se
+        </h6>
+
+        </div>
+      </div>
+
+    </div>
+
+
+    <!-- STYLEGUIDE -->
+
+    <div class="styleguide-grid">
+
+
+        <!-- LOGO -->
+
+        <section class="sgd-s-logo-display "  id="to-sgd-logo">
+          <div class="sgd-gc">
+
+            <div class="sgd-g-12">
+              <h2 class="sgd-header -type -brand">Logo</h2>
+            </div>
+
+            <div class="sgd-row">
+
+              <div class="sgd-g-12 card">
+         
+                 <svg version="1.1" id="Lager_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     width="300px" height="98.7px" viewBox="0 0 300 98.7" style="enable-background:new 0 0 300 98.7;" xml:space="preserve">
+                  <path d="M7.5,93.8h17.3V6.4H7.5V93.8L7.5,93.8z M31.1,93.8h17.5V52H49L59,93.8h12L81.1,52h0.3v41.8H99V6.4H75.6L65.3,52.6h-0.2
+                    L54.3,6.4H31.1V93.8L31.1,93.8z M105.3,93.8h18V58.4h8c9.2,0,17.9-4.2,17.8-16.4V23.6c0-8.5-5.1-17.2-15.4-17.2h-28.4V93.8
+                    L105.3,93.8z M123.3,23.2h2.5c5.9,0,7,3.5,7,9.6c0,5.5-1.3,8.4-6.9,8.4h-2.6V23.2L123.3,23.2z M142.7,93.8h17.7l2.1-11h13.7l2.1,11
+                    H196L178.5,6.4h-18.4L142.7,93.8L142.7,93.8z M169.2,44.8h0.2l3.5,21h-7L169.2,44.8L169.2,44.8z M196.6,72.8
+                    c0,14.3,9.2,22.4,21.8,22.4c13.7,0,22.4-6.6,22.4-22.4V57.5h-17.4v14.3c0,2.3-0.2,6.6-4.2,6.6c-4.5,0-4.5-4.3-4.5-6.6v-43
+                    c0-3,0.2-5.7,3.7-5.7c3.5,0,4.6,2.7,4.6,5.7v14.9h17.5V28.4c0-18.2-11.9-22.6-22.2-22.6c-10.9,0-21.8,6.6-21.8,20.9V72.8L196.6,72.8
+                    z M243.9,23.3h15.4v70.6H277V23.3h15V6.4h-48.1V23.3L243.9,23.3z"/>
+                  </svg>
+
+
+
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+
+        <!-- COLORS -->
+
+        <section class="sgd-s-colors -light -card-shadow"  id="to-sgd-colors">
+          <div class="sgd-gc">
+
+              <div class="sgd-g-12">
+                <h2 class="sgd-header -type -brand">Colors</h2>
+              </div>
+
+      
+
+              <div class="sgd-row">
+                <div class="sgd-g-3 card">
+                  <div class="sgd-color-card -black"><div class="body"></div><div class="shadow"></div></div>
+                </div>
+                <div class="sgd-g-3 card">
+                  <div class="sgd-color-card -yellow"><div class="body"></div><div class="shadow"></div></div>
+                </div>
+                <div class="sgd-g-3 card">
+                  <div class="sgd-color-card -grey"><div class="body"></div><div class="shadow"></div></div>
+                </div>
+
+              </div>
+
+
+
+          </div>
+        </section>
+
+
+        <!-- FONTS -->
+
+        <section class="sgd-s-typography " id="to-sgd-typography" >
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+              <h2 class="sgd-header -type -brand">Fonts</h2>
+            </div>
+
+
+            <div class="sgd-g-12 font font-impact ">
+              <div class="font-name">Impact</div>
+              <div class="font-glyphs">Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Xx Yy Zz Åå Ää Öö</div>
+              <div class="font-glyphs -pb">1234567890</div>
+
+              <div class="font-glyphs -italic">Italic Aa Bb Cc 123</div>
+              <div class="font-glyphs -bold -pb">Bold Aa Bb Cc 123</div>
+
+              <div class="font-code"><strong>$font-impact</strong>: "-apple-system", BlinkMacSystemFont, "Apple Color Emoji", “Segoe UI”, Roboto, “Helvetica Neue”, Helvetica, Arial, sans-serif;</div>
+            </div>
+
+            <div class="sgd-g-12 font font-circular">
+              <div class="font-name">Circular Std</div>
+              <div class="font-glyphs">Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Xx Yy Zz Åå Ää Öö</div>
+              <div class="font-glyphs -pb">1234567890</div>
+
+              <div class="font-glyphs -italic">Italic Aa Bb Cc 123</div>
+              <div class="font-glyphs -bold -pb">Bold Aa  B bCc 123</div>
+
+               <div class="font-code"><strong>$font-circular</strong>: "Circular-std", Arial, sans-serif;</div>
+            </div>
+
+
+
+          </div>
+        </section>
+
+
+        <!-- LINK -->
+
+        <section class="sgd-s-links  -light"  id="to-sgd-buttons">
+          <div class="sgd-gc">
+
+            <div class="sgd-g-12">
+              <h2 class="sgd-header -type -foundation">Link</h2>
+            </div>
+
+            <div class="sgd-g-12">
+              
+            </div>
+
+            <div class="sgd-row">
+              <div class="sgd-g-6">
+                <div><div class="sgd-header-tag">DEFAULT</div></div>
+                <a href="#">This is a default link</a>
+              </div>
+              
+              <div class="sgd-g-6">
+                <div><div class="sgd-header-tag">in text</div></div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a href="#" >Link in text</a> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit..</p>
+              </div>
+             
+            </div>
+
+          </div>
+        </section>
+
+
+        <!-- GRID -->
+
+        <section class="sgd-s-grid " id="to-sgd-grid">
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+              <h2 class="sgd-header -type -foundation">Layout - Breakpoints <span>(Px)</span></h2>
+            </div>
+            <div class="sgd-g-12">
+              <p>The most common breakpoints and highlited recommendations.</p> <br>
+              <div class="sgd-header-tag -large -rek">320</div>
+              <div class="sgd-header-tag -large">480</div>
+              <div class="sgd-header-tag -large">600</div>
+              <div class="sgd-header-tag -large -rek">768</div>
+              <div class="sgd-header-tag -large">1024</div>
+              <div class="sgd-header-tag -large -rek">1280</div>
+              <div class="sgd-header-tag -large">1368</div>
+              <div class="sgd-header-tag -large">1440</div>
+              <div class="sgd-header-tag -large">1680</div>
+              <div class="sgd-header-tag -large">1920</div>
+              <br><br>
+            </div>
+          </div>
+
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+              <h2 class="sgd-header -type -foundation">Layout - Grid <span>(12 columns, Px)</span></h2>
+            </div>
+          </div>
+
+          <div class="site-grid">
+            <div class="gc">
+
+              <div class="g-12"><div class="sgd-placeholder">g-12</div></div>
+
+              <div class="g-11"><div class="sgd-placeholder">g-11</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+
+              <div class="g-10"><div class="sgd-placeholder">g-10</div></div>
+              <div class="g-2"><div class="sgd-placeholder">g-2</div></div>
+
+              <div class="g-9"><div class="sgd-placeholder">g-9</div></div>
+              <div class="g-3"><div class="sgd-placeholder">g-3</div></div>
+
+              <div class="g-8"><div class="sgd-placeholder">g-8</div></div>
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+
+              <div class="g-7"><div class="sgd-placeholder">g-7</div></div>
+              <div class="g-5"><div class="sgd-placeholder">g-5</div></div>
+
+              <div class="g-6"><div class="sgd-placeholder">g-6</div></div>
+              <div class="g-6"><div class="sgd-placeholder">g-6</div></div>
+
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+              <div class="g-1"><div class="sgd-placeholder">g-1</div></div>
+
+              <div class="g-3 g-push-1 g-pull-8"><div class="sgd-placeholder">g-2 g-push-1</div></div>
+              <div class="g-2 g-m-push-0 g-t-push-0 g-push-3 g-pull-7"><div class="sgd-placeholder">g-4 g-push-3</div></div>
+
+            </div>
+
+            <div class="gc -g-center">
+              <div class="g-6"><div class="sgd-placeholder"> g-6</div></div>
+            </div>
+
+            <div class="gc -no-gutter">
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder -darker">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+            </div>
+
+            <div class="gc -full-width">
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder -darker">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+            </div>
+
+            <div class="gc -maximize">
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder -darker">g-4</div></div>
+              <div class="g-4"><div class="sgd-placeholder">g-4</div></div>
+            </div>
+
+            <div class="gc -g-center">
+              <div class="g-8 g-m-6 g-t-12"><div class="sgd-placeholder">g-8 g-m-6 g-t-12</div></div>
+            </div>
+
+          </div>
+        </section>
+
+
+        <!-- ICONS + FAVICON + IMAGES -->
+
+        <section class="sgd-s-icons sgd-s-images -light"  id="to-sgd-icons">
+          <div class="sgd-gc">
+
+
+            <div class="sgd-row">
+
+              <!-- ICONS -->
+              <div class="sgd-g-4">
+                 <h2 class="sgd-header -type -foundation">Icons</h2>
+                <svg-icon class="icon" name="facebook" />
+                <svg-icon class="icon" name="instagram" />
+              </div>
+
+              <!-- FAVICON  -->
+              <div class="sgd-g-4">
+                <h2 class="sgd-header -type -foundation">Favicon</h2>
+                <img src="~/static/android-chrome-512x512.png" class="fav">
+              </div>
+
+              <!-- IMAGES -->
+              <div class="sgd-g-4">
+                <h2 class="sgd-header -type -foundation">Image <span>(Ratio)</span> </h2>
+                <figure class="-image-ratio-4-3">
+                  <img src="https://images.prismic.io/impact/2236cde9-81ca-4f5e-a276-0459ef1dee1d_imp-p-2.jpeg?auto=compress,format&rect=0,0,5184,3453&w=512&h=341" />
+                </figure>
+              </div>
+
+
+            </div>
+
+          </div>
+        </section>
+
+
+        <!-- *** *** --- ----- ---  *** *** -->
+        <!-- *** *** --- PARTS ---  *** *** -->
+        <!-- *** *** --- ----- ---  *** *** -->
+
+
+        <!-- PART - HEADER -->
+
+        <section class="sgd-s-parts"  id="to-sgd-icons">
+
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+               <h2 class="sgd-header -type -part">Header</h2>
+            </div>
+          </div>
+
+          <div class="part">
+            <site-header />
+          </div>
+
+        </section>
+
+
+        <!-- PART - FOOTER -->
+
+        <section class="sgd-s-parts -light"  id="to-sgd-icons">
+
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+               <h2 class="sgd-header -type -part">Footer</h2>
+            </div>
+          </div>
+
+          <div class="part">
+            <site-footer />
+          </div>
+
+        </section>
+
+
+        <!-- PART - CONTENT -->
+
+        <section class="sgd-s-parts"  id="to-sgd-icons">
+
+          <div class="sgd-gc">
+            <div class="sgd-g-12">
+               <h2 class="sgd-header -type -part">CONTENT</h2>
+            </div>
+          </div>
+
+          <div class="part">
+            <br>
+            <section class="slice-impact-content impact-content -border-both"><div class="gc"><div class="g-12 border-top border"></div> <div class="row "><div class="g-6 slice-image"><img src="https://images.prismic.io/impact/7c79c67d-e4e1-42d0-84b4-5c4f71f2b01b_impact-temp-logo.jpg?auto=compress,format&amp;rect=0,0,3900,2597&amp;w=512&amp;h=341"> <!----></div> <div class="g-6 slice-content"><div class="header"><div class="header-styling"><h2>Thaiboxning</h2><h2></h2></div></div> <div class="text"><div><p><strong>NYBÖRJARGRUPPEN<br></strong><em><strong></strong>Junior: </em>3 pass/vecka, 2100kr, <a href="/content/schema">schema &gt;</a><br><em>Senior:</em> 3 pass/vecka, 3000kr, <a href="/content/schema">schema &gt;</a></p><p><strong>FORTSÄTTNINGSGRUPPEN<br></strong><em><strong></strong>Junior: </em>4 pass/vecka, 2100kr, <a href="/content/schema">schema &gt;</a><br><em>Senior:</em> 4 pass/vecka, 3000kr, <a href="/content/schema">schema &gt;</a></p><p><strong>AVANCERAT / TÄVLING<br></strong><em><strong></strong>6</em> pass/vecka, 3000kr, <a href="/content/schema">schema &gt;</a></p></div></div></div></div> <div class="g-12 border-bottom border"></div></div></section>
+          </div>
+
+        </section>
+
+
+
+
+
+
+     
+
+
+
+    </div>
+
+
+</section>
+</template>
+
+<script>
+
+  // import Logo from '~/components/Logo.vue'
+
+  export default {
+    components: {
+      // Logo
+    },
+    layout: 'placeholder'
+  }
+  
+</script>
+
+<style lang="scss">
+
+  @import "~/assets/scss/styleguide/styleguide-imports.scss";
+
+</style>
